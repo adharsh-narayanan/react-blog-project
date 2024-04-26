f52531d17d60f4eb91ae8c9fe2e8d74e8ee5b737
adharsh-narayanan <adharshnarayananay@gmail.com>
2024-04-26 10:57:13 +0530

    third

--- react-blog-app/src/App.jsx
+++ react-blog-app/src/App.jsx
@@ -1,5 +1,9 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import AllBlogs from './pages/AllBlogs'
 
 function App() {
 
@@ -7,8 +11,13 @@ function App() {
   return (
     <>
      <h1>blog</h1>
+     <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path='/Allblogs' element={<AllBlogs/>} />
+     </Routes>
 
-<h3>dddd</h3>
+
     </>
   )
 }
