53cb69a3684b35e6fdc41cc519b31e6d0124beb4
Abhirami Prakash <abhiramiharitha18@gmail.com>
2024-04-29 20:11:17 +0530

    Authentication

--- react-blog-app/src/components/Register.jsx
+++ react-blog-app/src/components/Register.jsx
@@ -1,17 +1,60 @@
-import React from 'react'
+
+
+import {Link} from 'react-router-dom';
+import mail from "./images/email.png";
+import lock from "./images/lock.png";
+import profile from "./images/icon.jpg";
+
 
 function Register() {
-  return (
-    <div>
-      <h2>Register</h2>
-      <form >
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  )
+    
+    
+
+    
+       
+
+    return (
+        <form >
+            <div className='main'>
+                <div className='sub-main'>
+                    <div>
+                        <div>
+                            <h1>Registration</h1>
+                            <div>
+                                <img src={profile} alt="emial" className='email' />
+                                <input type="text" placeholder='Enter Name' className='fill' />
+                            </div>
+                            <div className='mail-id'>
+                                <img src={mail} alt="emial" className='email' />
+                                <input type="email" placeholder='Enter Email-id' className='fill' />
+                            </div>
+                            <div className='mail-id'>
+                                <img src={lock} alt="emial" className='email' />
+                                <input type="password" placeholder='Enter New Password' className='fill' />
+                            </div>
+                            <div className='select'>
+                                {/* <label className='plist'>Intrest</label> */}
+                                    <select >
+                                        <option>Android Studio</option>
+                                        <option>ReactJS</option>
+                                        <option>Python</option>
+                                        <option>Machine Learning</option>
+                                    </select>
+                            </div>
+                            <div className='login-btn'>
+                                <Link to='/home'>
+                                    <button type="submit">Register</button>
+                                </Link>
+                            </div>
+                            <div className='reg-link'>
+                                <p>If Account exist then</p><Link className='link' to='/login'><li>Login!!!</li></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
 }
 
 export default Register
