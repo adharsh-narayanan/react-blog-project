c6fe34bb0aea85e1ac006abbae8a7c73889b4a2c
Abhiram007-creator <abhiramchulliyil@gmail.com>
2024-05-03 01:01:39 +0530

    allblogs

--- react-blog-app/src/pages/AllBlogs.jsx
+++ react-blog-app/src/pages/AllBlogs.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import Card from '../components/Card'
+import React, { useState } from 'react'
+import Data from '../components/Data'
+import Blog from '../components/Blog'
+import Categories from '../components/Categories'
 
 function AllBlogs() {
+  const [item,setItems] = useState(Data)
+  const menuItems = [...new Set(Data.map((val) => val.category))]
+
+  const filterItems =(cat) => {
+    const newItems = Data.filter((newval) => newval.category === cat)
+    setItems(newItems)
+  }
+   
   return (
-    <div>
-      <Card />
+    <div className='container-fluid'>
+      <div className='row'>
+        <h1 className='text-center col-12 fw-bold mat-3 mb-5'>All Blogs</h1>
+        <Categories 
+        menuItems = {menuItems}
+        filterItems={filterItems}
+        setItems={setItems}
+        />
+        <Blog item={item} />
+
+      </div>
+     
     </div>
   )
 }
